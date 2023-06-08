29ed5a260bbdb47250e6de3518b6c64506c7664a
iplux <ipluxteamx@github.com>
2023-06-08 21:23:18 +0200

    did i seriously not commit this

---
+++ OS2.2/API/gui/Textblock.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../includes/CpcdosAPI.h"
+
+#include "../../Cpcdos/include/sci.h"
+
+
+namespace CpcdosAPI {
+    Textblock::Textblock(std::string text, std::tuple<int, int> pos, std::tuple<int, int> size, std::tuple<int, int, int> textCol, std::tuple<int, int, int> backCol, std::string handle) {
+        textblockText = text;
+        textblockPosition = pos;
+        textblockSize = size;
+        textColor = textCol;
+        backgroundColor = backCol;
+        textboxHandle = handle;
+    }
+}
---
+++ OS2.2/API/gui/Window.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../includes/CpcdosAPI.h"
+
+#include "../../Cpcdos/include/sci.h"
+
+
+namespace CpcdosAPI {
+    Window::Window(std::string name, int type, std::tuple<int, int> size, std::tuple<int, int> pos, std::tuple<int, int, int, int> wincol, std::tuple<int, int, int> titlecol, std::string handle) {
+        windowName = name;
+        windowType = type;
+        windowSize = size;
+        windowPos = pos;
+        windowColor = wincol;
+        windowTitleColor = titlecol;
+        windowHandle = handle;
+    }
+}
