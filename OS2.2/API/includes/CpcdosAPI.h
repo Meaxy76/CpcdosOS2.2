51807b5b6325d2c42d6ca4670237f917b1e3124e
iplux <ipluxteamx@github.com>
2023-06-08 21:20:11 +0200

    Finish work on the Textblock component

--- OS2.2/API/includes/CpcdosAPI.h
+++ OS2.2/API/includes/CpcdosAPI.h
@@ -15,7 +15,7 @@
 
 namespace CpcdosAPI {
     class Window {
-        public:
+    public:
         std::string windowName;
         int windowType;
         std::tuple<int, int> windowSize;
@@ -27,6 +27,17 @@ namespace CpcdosAPI {
         Window(std::string name, int type, std::tuple<int, int> size, std::tuple<int, int> pos, std::tuple<int, int, int, int> wincol, std::tuple<int, int, int> titlecol, std::string handle);
     };
 
+    class Textblock {
+    public:
+        std::string textblockText;
+        std::tuple<int, int> textblockPosition;
+        std::tuple<int, int> textblockSize;
+        std::tuple<int, int, int> textColor;
+        std::tuple<int, int, int> backgroundColor;
+        std::string textboxHandle;
+        
+        Textblock(std::string text, std::tuple<int, int> pos, std::tuple<int, int> size, std::tuple<int, int, int> textCol, std::tuple<int, int, int> backCol, std::string handle);
+    };
 }
 
 
