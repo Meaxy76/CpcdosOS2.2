204e4a7afdda47de49e07e2e10959801354144f8
Meaxy <53201730+Meaxy76@users.noreply.github.com>
2023-07-06 12:50:39 -0400

    services.cpp を更新

--- OS2.2/Cpcdos/CpcdosCP/services.cpp
+++ OS2.2/Cpcdos/CpcdosCP/services.cpp
@@ -11,4 +11,32 @@ namespace SVC_CPC {
     int CpcSleep(unsigned int tms){
         return usleep(tms * 1000);
     }
+
+    /* Get time value */
+
+	int GetTime(int mode){
+	
+   		// current date/time based on current system
+      		time_t rawtime;
+
+     		struct tm *info;
+    		char buffer[80];
+    		time( &rawtime );
+    		info = localtime( &rawtime );
+
+    		switch(mode){
+    			case 12:
+    				std::cout << "12 hours selected" << std::endl;
+
+        			strftime(buffer,80,"%I:%M%p", info);
+         			std::cout << buffer << std::endl;
+    				return 0;
+      			case 24:
+      				std::cout << "24 hours selected" << std::endl;	
+    			
+          			strftime(buffer,80,"%H:%M", info);
+          			std::cout << buffer << std::endl;
+          			return 0;
+    		}
+    	}
 }
